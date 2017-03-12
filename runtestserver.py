b789970ff42e1484eeddb1afd683efd159343f59
reiinakano <reiichiro.s.nakano@gmail.com>
2017-03-13 01:46:15 +0800

    add flask skeleton files

---
+++ runtestserver.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import, print_function,\
+    nested_scopes, generators, division, with_statement, unicode_literals
+from xcessiv import app
+import sys
+
+
+if __name__ == '__main__':
+    port = 8080 if len(sys.argv) < 2 else sys.argv[1]
+    app.run(debug=True, port=int(port), host='0.0.0.0')
--- xcessiv/views.py
+++ xcessiv/views.py
@@ -1,10 +1,3 @@
 from __future__ import absolute_import, print_function,\
     nested_scopes, generators, division, with_statement, unicode_literals
-from flask import Flask
-
-
-app = Flask(__name__)
-app.config.from_object('config')
-
-
-import xcessiv.views
+from xcessiv import app
