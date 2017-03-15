32078c84638df09599ac5515ffa7cffb651eb096
reiinakano <reiichiro.s.nakano@gmail.com>
2017-03-16 02:11:44 +0800

    add views to GET and PATCH test dataset extraction and meta-feature generation method

--- xcessiv/views.py
+++ xcessiv/views.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, print_function, division, unicode_litera
 import os
 from flask import request, jsonify, abort
 from xcessiv import app, constants, functions
+import six
 
 
 def my_message(message, code=200):
@@ -28,7 +29,7 @@ def create_new_ensemble():
 
 
 @app.route('/ensemble/extraction/main-dataset/<path:path>/', methods=['GET', 'PATCH'])
-def main_dataset_source(path):
+def extraction_main_dataset(path):
     try:
         xcnb = functions.read_xcnb(path)
     except IOError:
@@ -39,6 +40,44 @@ def main_dataset_source(path):
 
     if request.method == 'PATCH':
         req_body = request.get_json()
-        xcnb["extraction"]["main_dataset"]["source"] = req_body["source"]
+        for key, value in six.iteritems(req_body):
+            xcnb['extraction']['main_dataset'][key] = value
         functions.write_xcnb(path, xcnb)
         return my_message("Updated main dataset extraction")
+
+
+@app.route('/ensemble/extraction/test-dataset/<path:path>/', methods=['GET', 'PATCH'])
+def extraction_test_dataset(path):
+    try:
+        xcnb = functions.read_xcnb(path)
+    except IOError:
+        abort(404)
+
+    if request.method == 'GET':
+        return jsonify(xcnb['extraction']['test_dataset'])
+
+    if request.method == 'PATCH':
+        req_body = request.get_json()
+        for key, value in six.iteritems(req_body):
+            xcnb['extraction']['test_dataset'][key] = value
+        functions.write_xcnb(path, xcnb)
+        return my_message("Updated test dataset extraction")
+
+
+@app.route('/ensemble/extraction/meta-feature-generation/<path:path>/',
+           methods=['GET', 'PATCH'])
+def extraction_meta_feature_generation(path):
+    try:
+        xcnb = functions.read_xcnb(path)
+    except IOError:
+        abort(404)
+
+    if request.method == 'GET':
+        return jsonify(xcnb['extraction']['meta_feature_generation'])
+
+    if request.method == 'PATCH':
+        req_body = request.get_json()
+        for key, value in six.iteritems(req_body):
+            xcnb['extraction']['meta_feature_generation'][key] = value
+        functions.write_xcnb(path, xcnb)
+        return my_message("Updated meta-feature generation")
