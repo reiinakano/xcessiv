39104d7dccb7327f9d9ce5e2e8bc41391ac2b7d8
reiinakano <reiichiro.s.nakano@gmail.com>
2017-04-03 22:04:43 +0800

    delete redundant CodeBox component

--- ui/src/DatasetExtraction/MainDataExtraction.js
+++ ui/src/DatasetExtraction/MainDataExtraction.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import CodeBox from '../CodeBox';
 import './MainDataExtraction.css';
+import CodeMirror from 'react-codemirror';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/mode/python/python';
 
 class MainDataExtraction extends Component {
 
@@ -62,7 +64,7 @@ class MainDataExtraction extends Component {
   	return <div className='MainDataExtraction'>
   	  <h2> Main Dataset Extraction Setup </h2>
   	  <h3> Main Dataset Extraction Source </h3>
-  	  <CodeBox defaultText={this.state.newCode} onChange={this.newSource}/>
+  	  <CodeMirror value={this.state.newCode} onChange={this.newSource}/>
   	  <button disabled={this.state.same} onClick={this.saveSetup}> Save Main Dataset Extraction Setup </button>
   	</div>
   }
