55c7a41922b8064599e1caa40b8a3cdaf2715e4c
reiinakano <reiichiro.s.nakano@gmail.com>
2017-04-28 21:17:12 +0800

    complete layout of ensemble builder

--- ui/src/Ensemble/EnsembleBuilder.js
+++ ui/src/Ensemble/EnsembleBuilder.js
@@ -5,18 +5,33 @@ import 'react-virtualized/styles.css'
 import 'react-virtualized-select/styles.css'
 import VirtualizedSelect from 'react-virtualized-select'
 import Select from 'react-select'
+import CodeMirror from 'react-codemirror';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/mode/python/python';
+
+const defaultSourceParams = [
+  '"""This code block should ',
+  'contain the hyperparameters to be used for ',
+  'the secondary base learner in the variable `params`"""\n',
+  'params = {}'
+].join('')
 
 class EnsembleBuilder extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      selectedValue: null 
+      selectedValue: null,
+      source: defaultSourceParams,
+      appendOriginal: false
     };
   }
 
   render() {
-    console.log(this.state.selectedValue);
+    var options = {
+      lineNumbers: true,
+      indentUnit: 4
+    };
 
     return (
       <div className='Ensemble'>
@@ -35,6 +50,19 @@ class EnsembleBuilder extends Component {
           onChange={(val) => this.setState({selectedValue: val})}
           placeholder="Select secondary base learner to use"
         />
+        <CodeMirror 
+          value={this.state.source} 
+          onChange={(src) => this.setState({source: src})} 
+          options={options}
+        />
+        <label>
+          <input type="checkbox" 
+            checked={this.state.appendOriginal} 
+            onChange={() => this.setState((prevState) => 
+              ({appendOriginal: !prevState.appendOriginal}))}
+          />
+          Append original features to secondary features
+        </label>
       </div>
     )
   }
