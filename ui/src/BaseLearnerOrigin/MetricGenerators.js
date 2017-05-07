0af8dd0426c31e97528e7bbf3e8388f641ba7cef
reiinakano <reiichiro.s.nakano@gmail.com>
2017-05-07 17:39:44 +0800

    adjust modals and buttons in MetricGenerators

--- ui/src/BaseLearnerOrigin/MetricGenerators.js
+++ ui/src/BaseLearnerOrigin/MetricGenerators.js
@@ -6,8 +6,8 @@ import 'codemirror/mode/python/python';
 import 'rc-collapse/assets/index.css';
 import {omit} from 'lodash';
 import Collapse, { Panel } from 'rc-collapse';
-import ReactModal from 'react-modal';
-import { Button, Glyphicon } from 'react-bootstrap';
+import { Button, Glyphicon, Modal, Form, FormGroup, 
+  FormControl, ControlLabel } from 'react-bootstrap';
 
 const default_metric_generator_code = `def metric_generator(y_true, y_probas):
     """This function must return a numerical value given two numpy arrays 
@@ -17,75 +17,48 @@ const default_metric_generator_code = `def metric_generator(y_true, y_probas):
     return 0.88
 `
 
-const modalStyle = {
-  overlay : {
-    zIndex            : 1000
-  },
-  content : {
-    top                        : '50%',
-    left                       : '50%',
-    right                      : 'auto',
-    bottom                     : 'auto',
-    marginRight                : '-50%',
-    transform                  : 'translate(-50%, -50%)',
-    border                     : '1px solid #ccc',
-    background                 : '#fff',
-    overflow                   : 'auto',
-    WebkitOverflowScrolling    : 'touch',
-    borderRadius               : '4px',
-    outline                    : 'none',
-    padding                    : '20px'
-  }
-}
-
 class AddNewModal extends Component {
   constructor(props) {
     super(props);
     this.state = {
       name: ''
     };
-    this.handleNameChange = this.handleNameChange.bind(this);
-  }
-
-  handleNameChange(event) {
-    console.log(event.target.value);
-    this.setState({name: event.target.value});
   }
 
   render() {
     return (
-      <ReactModal 
-        isOpen={this.props.isOpen} 
-        onRequestClose={this.props.onRequestClose}
-        contentLabel='Add new metric generator'
-        style={modalStyle}
+      <Modal 
+        show={this.props.isOpen} 
+        onHide={this.props.onRequestClose}
       >
-        <p>Name new metric</p>
-        <label>
-          Name: 
-          <input type='text' value={this.state.name} 
-          onChange={this.handleNameChange} />
-        </label>
-        <button onClick={this.props.onRequestClose}>Cancel</button>
-        <button onClick={() => this.props.onAdd(this.state.name)}>Add</button>
-      </ReactModal>
+        <Modal.Header closeButton>
+          <Modal.Title>Add new metric generator</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <FormGroup
+              controlId='metricName'
+            >
+              <ControlLabel>Name new metric</ControlLabel>
+              <FormControl
+                value={this.state.name}
+                onChange={(evt) => this.setState({name: evt.target.value})}            
+              />
+            </FormGroup>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this.props.onRequestClose}>Cancel</Button>
+          <Button 
+            bsStyle='primary' 
+            onClick={() => this.props.onAdd(this.state.name)}
+          >
+            Yes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     )
   }
-}
-
-function DeleteModal(props) {
-  return (
-    <ReactModal 
-      isOpen={props.isOpen} 
-      onRequestClose={props.onRequestClose}
-      contentLabel='Delete metric generator'
-      style={modalStyle}
-    >
-      <p>Are you sure you want to delete this metric generator?</p>
-      <button onClick={props.onRequestClose}>Cancel</button>
-      <button onClick={props.onDelete}>Yes</button>
-    </ReactModal>
-  )
 }
 
 class MetricGenerators extends Component {
@@ -96,54 +69,45 @@ class MetricGenerators extends Component {
       showAddNewModal: false,
       showDeleteModal: false
     };
-    this.handleOpenDeleteModal = this.handleOpenDeleteModal.bind(this);
-    this.handleCloseDeleteModal = this.handleCloseDeleteModal.bind(this);
-    this.handleChangeMetricGenerator = this.handleChangeMetricGenerator.bind(this);
-    this.handleDeleteMetricGenerator = this.handleDeleteMetricGenerator.bind(this);
   }
 
   /* Used to construct a list of Generator components for inserting into the 
   accordion.
   */
   getItems() {
-    const items = [];
-    for (var key in this.props.generators) {
-      console.log(key);
-      
-      var options = {
-        lineNumbers: true,
-        indentUnit: 4,
-        readOnly: this.props.disabled
-      };
-      items.push(<Panel key={key} header={key}>
-        <CodeMirror value={this.props.generators[key]} 
-        onChange={this.handleChangeMetricGenerator.bind(null, key)} 
-        options={options}/>
-        <button disabled={this.props.disabled} 
-        onClick={this.handleOpenDeleteModal}>Delete</button>
-        <DeleteModal isOpen={this.state.showDeleteModal} 
-        onRequestClose={this.handleCloseDeleteModal}
-        onDelete={this.handleDeleteMetricGenerator.bind(null, key)} />
-      </Panel>)
-    }
+
+    var options = {
+      lineNumbers: true,
+      indentUnit: 4,
+      readOnly: this.props.disabled
+    };
+
+    const items = Object.keys(this.props.generators).map((key) => {
+      var header = (
+        <b>
+          {key}
+          {!this.props.disabled && (
+            <a
+              className='DeleteButton'
+              onClick={(evt) => {
+                evt.stopPropagation();
+                this.handleDeleteMetricGenerator(key);
+              }}>
+              <Glyphicon glyph="remove" />
+            </a>
+          )}
+        </b>
+      );
+      return (
+        <Panel key={key} header={header}>
+          <CodeMirror value={this.props.generators[key]} 
+          onChange={(src) => this.handleChangeMetricGenerator(key, src)} 
+          options={options}/>
+        </Panel>
+      )
+    })
 
     return items;
-  }
-
-  // Handler when active panel changes
-  onActiveChange(activeKey) {
-    console.log(activeKey);
-    this.setState({
-      activeKey
-    });
-  }
-
-  handleOpenAddNewModal() {
-    this.setState({showAddNewModal: true});
-  }
-
-  handleCloseAddNewModal() {
-    this.setState({showAddNewModal: false});
   }
 
   handleChangeMetricGenerator(metric_name, source) {
@@ -162,14 +126,6 @@ class MetricGenerators extends Component {
     this.setState({showAddNewModal: false});
   }
 
-  handleOpenDeleteModal() {
-    this.setState({showDeleteModal: true});
-  }
-
-  handleCloseDeleteModal() {
-    this.setState({showDeleteModal: false});
-  }
-
   handleDeleteMetricGenerator(metric_name) {
     if (metric_name in this.props.generators) {
       var newGenerators = omit(this.props.generators, metric_name);
@@ -183,18 +139,18 @@ class MetricGenerators extends Component {
       <div>
         <h4>Metrics to be calculated from meta-features</h4>
         <Collapse activeKey={this.state.activeKey} 
-        onChange={(activeKey) => this.onActiveChange(activeKey)}
+        onChange={(activeKey) => this.setState({activeKey})}
         accordion={false}>
           {this.getItems()}
         </Collapse>
         <Button block 
           disabled={this.props.disabled}
-          onClick={() => this.handleOpenAddNewModal()}>
+          onClick={() => this.setState({showAddNewModal: true})}>
           <Glyphicon glyph="plus" />
           {' Add new metric generator'}
         </Button>
         <AddNewModal isOpen={this.state.showAddNewModal} 
-        onRequestClose={() => this.handleCloseAddNewModal()}
+        onRequestClose={() => this.setState({showAddNewModal: false})}
         onAdd={(metric_name) => this.handleAddMetricGenerator(metric_name)} />
       </div>
     );
