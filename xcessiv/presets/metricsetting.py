653505bb4e63fb80437a059a3514d884698066ac
reiinakano <reiichiro.s.nakano@gmail.com>
2017-05-09 21:43:48 +0800

    precision_from_scores, precision_from_preds

--- xcessiv/presets/metricsetting.py
+++ xcessiv/presets/metricsetting.py
@@ -23,7 +23,6 @@ def metric_generator(y_true, y_probas):
 """
 }
 
-
 accuracy_from_preds = {
     'name': 'Accuracy',
     'source':
@@ -36,7 +35,6 @@ def metric_generator(y_true, y_preds):
     return accuracy_score(y_true, y_preds)
 """
 }
-
 
 recall_from_scores = {
     'name': 'Recall',
@@ -80,6 +78,52 @@ def metric_generator(y_true, y_preds):
         score = recall_score(y_true, y_preds, average='micro')
     else:
         score = recall_score(y_true, y_preds)
+    return score
+"""
+}
+
+precision_from_scores = {
+    'name': 'Precision',
+    'source':
+    """from sklearn.metrics import precision_score
+import numpy as np
+
+def metric_generator(y_true, y_probas):
+    \"\"\"This function computes the precision given the true labels array (y_true)
+    and the scores/probabilities array (y_probas) with shape (num_samples, num_classes).
+    For the function to work correctly, the columns of the probabilities array must
+    correspond to a sorted set of the unique values present in y_true. If there are more than
+    two classes, micro-averaging is used by default.
+    \"\"\"
+    classes_ = np.unique(y_true)
+    if len(classes_) != np.array(y_probas).shape[1]:
+        raise ValueError('The shape of y_probas does not correspond to the number of unique values in y_true')
+    argmax = np.argmax(y_probas, axis=1)
+    y_preds = classes_[argmax]
+    if np.array(y_probas).shape[1] > 2:
+        score = precision_score(y_true, y_preds, average='micro')
+    else:
+        score = precision_score(y_true, y_preds)
+    return score
+"""
+}
+
+
+precision_from_preds = {
+    'name': 'Precision',
+    'source':
+    """from sklearn.metrics import precision_score
+import numpy as np
+
+def metric_generator(y_true, y_preds):
+    \"\"\"This function computes the precision given the true labels array (y_true)
+    and the predicted labels array (y_preds).
+    \"\"\"
+    classes_ = np.unique(y_true)
+    if len(classes_) > 2:
+        score = precision_score(y_true, y_preds, average='micro')
+    else:
+        score = precision_score(y_true, y_preds)
     return score
 """
 }
