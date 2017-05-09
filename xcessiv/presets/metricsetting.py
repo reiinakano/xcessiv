1a1255bd4ce915e146dff665dc5acf758ad7e9a0
reiinakano <reiichiro.s.nakano@gmail.com>
2017-05-09 21:47:33 +0800

    f1_score_from_scores, f1_score_from_preds

--- xcessiv/presets/metricsetting.py
+++ xcessiv/presets/metricsetting.py
@@ -127,3 +127,49 @@ def metric_generator(y_true, y_preds):
     return score
 """
 }
+
+f1_score_from_scores = {
+    'name': 'F1 Score',
+    'source':
+    """from sklearn.metrics import f1_score
+import numpy as np
+
+def metric_generator(y_true, y_probas):
+    \"\"\"This function computes the F1 score given the true labels array (y_true)
+    and the scores/probabilities array (y_probas) with shape (num_samples, num_classes).
+    For the function to work correctly, the columns of the probabilities array must
+    correspond to a sorted set of the unique values present in y_true. If there are more than
+    two classes, micro-averaging is used by default.
+    \"\"\"
+    classes_ = np.unique(y_true)
+    if len(classes_) != np.array(y_probas).shape[1]:
+        raise ValueError('The shape of y_probas does not correspond to the number of unique values in y_true')
+    argmax = np.argmax(y_probas, axis=1)
+    y_preds = classes_[argmax]
+    if np.array(y_probas).shape[1] > 2:
+        score = f1_score(y_true, y_preds, average='micro')
+    else:
+        score = f1_score(y_true, y_preds)
+    return score
+"""
+}
+
+
+f1_score_from_preds = {
+    'name': 'F1 Score',
+    'source':
+    """from sklearn.metrics import f1_score
+import numpy as np
+
+def metric_generator(y_true, y_preds):
+    \"\"\"This function computes the F1 score given the true labels array (y_true)
+    and the predicted labels array (y_preds).
+    \"\"\"
+    classes_ = np.unique(y_true)
+    if len(classes_) > 2:
+        score = f1_score(y_true, y_preds, average='micro')
+    else:
+        score = f1_score(y_true, y_preds)
+    return score
+"""
+}
