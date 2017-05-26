678a1b3bd654d7d8648f4bb18ea8c43840dd377f
reiinakano <reiichiro.s.nakano@gmail.com>
2017-05-26 21:39:12 +0800

    update pypi

--- xcessiv/__init__.py
+++ xcessiv/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, print_function, division, unicode_litera
 from flask import Flask
 
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 
 app = Flask(__name__, static_url_path='/static', static_folder='ui/build/static')
