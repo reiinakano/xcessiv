bd1ec3473afd929f6334629bf6e9015fa669a917
reiinakano <reiichiro.s.nakano@gmail.com>
2017-05-28 14:38:33 +0800

    change docs

--- xcessiv/constants.py
+++ xcessiv/constants.py
@@ -1,3 +1,6 @@
+from xcessiv.presets.cvsetting import k_fold
+
+
 extraction_default_source = """\"\"\"In this code block, you must define the function `extract_{}_dataset`.
 `extract_{}_dataset` must take no arguments and return a tuple (X, y), where X is a
 Numpy array with shape (n_samples, n_features) corresponding to the features of your
@@ -9,14 +12,7 @@ def extract_{}_dataset():
     return [[1, 2], [2, 1]], [0, 1]
 """
 
-meta_feature_generation_default_source = """\"\"\"In this code block, you must define the function
-`return_splits_iterator`. `return_splits_iterator` must take two arguments `X` and `y`, where X is a
-Numpy array with shape (n_samples, n_features) corresponding to the features of your
-{} dataset and y is the Numpy array corresponding to the ground truth labels of each
-sample. It must return an iterable that contains train-test splits that can be used by `X` and `y`.
-\"\"\"
-
-"""
+meta_feature_generation_default_source = k_fold['source']
 
 DEFAULT_EXTRACTION_MAIN_DATASET = {
     "source": extraction_default_source.format('main', 'main', 'main', 'main')
